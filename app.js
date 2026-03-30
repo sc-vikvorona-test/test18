16b5aa9092a2744bc834d3b38170c7476f986223
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-30 16:13:42 +0200

    chore: validate log prefix fix deployment

--- app.js
+++ app.js
@@ -15,3 +15,4 @@ document.getElementById('msg-btn').addEventListener('click', () => {
   const msg = document.getElementById('msg-input').value;
   document.getElementById('msg-output').textContent = msg;
 });
+// validate log prefix fix - Mon Mar 30 16:13:36 CEST 2026
